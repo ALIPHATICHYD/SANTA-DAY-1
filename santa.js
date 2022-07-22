16515733368c105d274ade05c6ee7fa73b5c654a
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-07-23 00:42:19 +0100

    Update santa.js

--- santa.js
+++ santa.js
@@ -2,3 +2,15 @@ const fs = require('fs');
 // The floor santa ends up on
 // ( -- up one floor
 // ) -- down one floor
+
+const santaFunction = () => {
+    fs.readFile('./santa.txt', (err, data) => {
+        const santaDirections = data.toString();
+        const santaDirectionsArray = santaDirections.split('');
+        const floor = santaDirectionsArray.reduce((acc, bracket) => {
+            if (bracket === '('){
+                return acc +=1;
+            } else if(bracket === ')'){
+                return acc -=1;
+            }
+            }, 0);
